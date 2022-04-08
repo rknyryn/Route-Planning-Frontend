eab246822f432b120040c831e9a5ed19667acd7b
rknyryn <rknyryn@gmail.com>
2022-04-09 00:07:30 +0300

    add screen for admin

---
+++ screens/HomeAdminScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HomeAdminScreen() {
+  return (
+    <View>
+      <Text>HomeAdminScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
