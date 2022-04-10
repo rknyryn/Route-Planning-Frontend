277dd3fa96a00d7a1aacc58e5af280608cbceff3
rknyryn <rknyryn@gmail.com>
2022-04-10 12:53:01 +0300

    add stations data request

--- screens/HomeUserScreen.js
+++ screens/HomeUserScreen.js
@@ -1,30 +1,59 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native'
 import { Picker } from '@react-native-picker/picker';
 
 export default function HomeUser({ navigation }) {
   const [selectedData, setSelectedData] = useState();
+  const [stations, setStations] = useState([]);
+
+  async function getAllStation() {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "ARRAffinity=6507bdc255f23a4f1ad4b5182514791bd1126448d921d867fd42e77489564d58");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    await fetch("http://route-planning-backend.azurewebsites.net/station/list", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        setStations(result.station_list);
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  useEffect(() => {
+    getAllStation();
+  }, []);
+
+  const renderStationList = () => {
+    return stations.map((station) => {
+      return <Picker.Item label={station.name} value={station} key={station.id} />
+    })
+  }
 
   return (
     <View style={styles.container}>
-      <View style={{ margin:5, width: "80%", backgroundColor: "white", borderRadius: 10 }}>
+      <View style={{ margin: 5, width: "80%", backgroundColor: "white", borderRadius: 10 }}>
         <Picker
           selectedValue={selectedData}
-          onValueChange={(itemValue, itemIndex) =>
+          onValueChange={(itemValue, itemIndex) => {
             setSelectedData(itemValue)
+          }
           }>
-          <Picker.Item label="İzmit" value="izmit" />
-          <Picker.Item label="Gölcük" value="gölcük" />
+          {stations && renderStationList()}
         </Picker>
       </View>
       <TouchableOpacity
         style={styles.button}
+        onPress={() => console.log(selectedData)}
       >
         <Text style={styles.buttonText}>Save</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate('Map')}
       >
         <Text style={styles.buttonText}>Route</Text>
       </TouchableOpacity>
@@ -50,5 +79,14 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 })
