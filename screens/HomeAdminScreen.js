cd3d0aff32dc9e8cc4fda729b8da05cc7751ac41
rknyryn <rknyryn@gmail.com>
2022-04-11 17:47:50 +0300

    add stations screen and daily vote screen

--- screens/HomeAdminScreen.js
+++ screens/HomeAdminScreen.js
@@ -11,125 +11,22 @@ import {
 } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import StationsScreen from './StationsScreen';
+import DailyVoteScreen from './DailyVoteScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeAdminScreen() {
   return (
-    <Tab.Navigator initialRouteName="Settings">
-      <Tab.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-      <Tab.Screen options={{ headerShown: false }} name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="DailyVote">
+      <Tab.Screen name="DailyVote" component={DailyVoteScreen} />
+      <Tab.Screen name="Stations" component={StationsScreen} />
     </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF"
-  },
-  containerList: {
-    flex: 1,
-    margin: 20,
-  },
-  item: {
-    backgroundColor: '#FFF',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  floatingButton: {
-    position: 'absolute',
-    width: 65,
-    height: 65,
-    borderRadius: 35,
-    alignItems: 'center',
-    justifyContent: 'center',
-    right: 10,
-    bottom: 10,
-    backgroundColor: "#000",
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  floatingButtonText: {
-    color: "#FFF",
-    fontWeight: "700",
-    fontSize: 26
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-    backgroundColor: "#202020" + "AA",
-  },
-  modalView: {
-    width: "90%",
-    height: 300,
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalInput: {
-    backgroundColor: '#DFDFDF',
-    width: "90%",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  modalButton: {
-    height: 50,
-    width: 100,
-    borderRadius: 10,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  modalButtonText: {
-    color: "#FFF",
-    fontSize: 16,
-    fontWeight: "700"
-  }
+  
 })
